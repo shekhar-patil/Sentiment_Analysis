d11f69e42644d14b6cf3b11a33093835b8929286
shekhar-patil <patilshekhar900@gmail.com>
2020-03-01 15:27:14 +0530

    Add styling

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,14 @@
 module HomeHelper
+  def polarity_css_class(polarity)
+    case polarity
+    when 'positive'
+      'p-3 mb-2 bg-success'
+    when 'negative'
+      'p-3 mb-2 bg-danger'
+    when 'neutral'
+      'p-3 mb-2 bg-warning'
+    else
+      'p-3 mb-2 bg-info'
+    end
+  end
 end
